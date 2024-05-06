5814b635e9c47e3bbd30c44efc6b02be58ece328
rm86626 <rm@qhdlink.com>
2024-05-06 14:20:07 +0800

    国际新闻组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <h3>App组件</h3>
+      <MyNews></MyNews>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import MyNews from './MyNews'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { MyNews },
+    name:"App"
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
